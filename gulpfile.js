ccb14e691407f952fa7bb6b64c76607780a9299a
Elliot Collin <elliot.cln@gmail.com>
2018-03-11 15:17:58 -0400

    Refactoring assets->src + set scss helpers + optimize gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
-var gulp = require('gulp'),
-	cssnano = require('gulp-cssnano'),
-	autoprefixer = require('gulp-autoprefixer'),
-	rename = require('gulp-rename'),
-	sass = require('gulp-sass');
+// Define paths
+var path = {
+	css: 'src/css/',
+	scss: 'src/scss/',
+	dist: 'dist/'
+}
 
+// Requires
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')();
+
+//  Tasks
 gulp.task('scss', function(){
-	return gulp.src('assets/scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('assets/css'));
+	return gulp.src(path.scss + '**/*.scss')
+		.pipe($.sass().on('error', $.sass.logError))
+		.pipe(gulp.dest(path.css));
 });
 
 gulp.task('css-prod', function(){
 	// Rename and minify css
-	return gulp.src('assets/css/*.css')
-	.pipe(autoprefixer())
-	.pipe(cssnano())
-	.pipe(rename(function (path) {
+	return gulp.src(path.css + '*.css')
+	.pipe($.autoprefixer())
+	.pipe($.cssnano())
+	.pipe($.rename(function (path) {
     path.basename += ".min";
   }))
-	.pipe(gulp.dest('dist/css'));
+	.pipe(gulp.dest(path.dist + 'css'));
 });
 
 gulp.task('prod', ['css-prod'], function(){
-	return gulp.src('assets/css/*.css')
-	.pipe(autoprefixer())
-	.pipe(gulp.dest('dist/css'));
+	return gulp.src(path.css + '*.css')
+	.pipe($.autoprefixer())
+	.pipe(gulp.dest(path.dist + 'css'));
 });
 
 gulp.task('default', ['scss'], function(){});
 
 gulp.task('watch', function(){
-	gulp.watch('assets/scss/**/*.scss', ['scss']);
+	gulp.watch(path.scss + '**/*.scss', ['scss']);
 });
